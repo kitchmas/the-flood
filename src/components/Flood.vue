449b8ec628e74c781f946b11746d78acb2841176
Thomas Kitch <thomaswkitch@gmail.com>
2020-02-16 10:20:03 +1300

    new vue project with vuex

---
+++ src/components/Flood.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="frame">
+    <h1>{{title}}</h1>
+    {{health}}
+    {{stamina}}
+    <button  @click="attacked(damage)">damage</button>
+    {{damage}}
+    <div id="game-wrapper"></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState, mapMutations } from 'vuex'
+
+export default {
+  name: 'Flood',
+   computed: {
+    ...mapState({
+      health: state => state.game.health,
+      stamina: state  => state.game.stamina
+    }),
+    ...mapGetters('game', {
+       damage: 'getHealthLost'
+    })
+  },
+  methods:{
+     ...mapMutations({
+      attacked: 'game/attacked' // map `this.add()` to `this.$store.commit('increment')`
+    }),
+    // attack (damage) {
+      // console.log("hi")
+      // attacked(damage);
+      // this.$store.commit('attacked',damage)
+    // }
+  },
+  data:function(){
+    return{
+    title:"The Flood"
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  text-align: center;
+}
+
+#game-wrapper{
+  height: 470px;
+  width: 470px;
+  margin: 0 auto;
+  border: 2px solid black;
+}
+
+</style>
---
+++ src/store/modules/game.js
@@ -0,0 +1,36 @@
+
+// initial state
+// shape: [{ id, quantity }]
+const state = {
+  health:10,
+  stamina: 10,
+  maxHealth:0,
+  maxStamina:0
+}
+
+// getters
+const getters = {
+  getHealthLost: (state) => {
+      return state.maxHealth  - state.health;
+  }
+}
+
+// actions (used for async)
+const actions = {
+
+}
+
+// mutations
+const mutations = {
+  attacked (state, damage) {
+    state.health = state.health -  damage
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
